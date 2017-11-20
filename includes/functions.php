ecc3b0f0d16abe439277dc734de7752657889aa7
Bunias <PJOpozda@gmail.com>
2017-11-20 12:27:19 +0000

    Add helper function to includes/functions.php for simple including layout templates from html pages

--- includes/functions.php
+++ includes/functions.php
@@ -29,4 +29,9 @@ function __autoload($class_name)
   file_exists($path) ? require_once($path) : die("The file {$class_name}.php could not be found.");
 }
 
+function include_layout_template($template="")
+{
+  include(SITE_ROOT.DS."public".DS."layouts".DS.$template);
+}
+
 ?>
