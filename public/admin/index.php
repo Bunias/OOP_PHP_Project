ad0ec576accda7d07648566cb739ca0b5f76d527
Bunias <PJOpozda@gmail.com>
2017-11-20 12:31:26 +0000

    Modify index.php for admin area to use created layouts

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,26 +1,15 @@
 <?php
 
-require_once('../../includes/initialize.php');
+require_once("../../includes/initialize.php");
 
 !$session->is_logged_in() ? redirect_to("login.php") : null;
 
 ?>
 
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>Photo Gallery</title>
-    <link href="../css/main.css" media="all" rel="stylesheet" type="text/css"/>
-  </head>
-  <body>
-    <div id="header">
-      <h1>Photo Gallery</h1>
-    </div>
-    <div id="main">
+
+<?php include_layout_template("admin_header.php"); ?>
+
       <h2>Menu</h2>
-    </div>
-    <div id="footer">
-      Copyright <?php echo date("Y", time()); ?>, Piotr Opozda
-    </div>
-  </body>
-</html>
+      
+<?php include_layout_template("admin_footer.php"); ?>
